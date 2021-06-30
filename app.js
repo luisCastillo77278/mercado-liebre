0bc0d0b7d3285850b69f7c08863027145c01df36
luis castillo <lc77278@gmail.com>
2021-06-30 18:54:30 -0500

    modify app.js

--- app.js
+++ app.js
@@ -18,4 +18,4 @@ app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/login.html'));
 })
 
-app.listen(process.env.PORT || 3000, () => console.log('Servidor corriendo en el puerto 3000'));
+app.listen((process.env.PORT || 3000), () => console.log('Servidor corriendo en el puerto 3000'));
