ec28cfdc238102c2eff5e8b1ef005f64120e76b5
luis castillo <lc77278@gmail.com>
2021-06-30 18:35:08 -0500

    deploy

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/index.html'));
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/register.html'));
+})
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/login.html'));
+})
+
+app.listen(process.env.PORT || 3000, () => console.log('Servidor corriendo en el puerto 3000'));
